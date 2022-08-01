051f9c2e321d16c83d21014fb903cd94bb5080e9
Jason Turner <jason@emptycrate.com>
2022-08-01 17:27:32 -0600

    Make sure lld is enabled

--- build_package.sh
+++ build_package.sh
@@ -2,7 +2,7 @@ git clone --depth 1 --branch llvmorg-10.0.1 git@github.com:llvm/llvm-project llv
 
 patch -p1 -d llvm-project -i ../llvm-msvc.patch
 
-cmake -G "Visual Studio 17 2022" -A x64 -T "host=x64" -S llvm-project/llvm -B llvm-build -D LLVM_ENABLE_PROJECTS:STRING="clang;lld;libc" -D LLVM_BUILD_TESTS:BOOL=OFF -D LLVM_TARGETS_TO_BUILD:STRING="X86"
+cmake -G "Visual Studio 17 2022" -A x64 -T "host=x64" -S llvm-project/llvm -B llvm-build -D LLVM_ENABLE_PROJECTS:STRING="clang;lld;libc" -D LLVM_BUILD_TESTS:BOOL=FALSE -D LLVM_TARGETS_TO_BUILD:STRING="X86" -D LLVM_TOOL_LLD_BUILD:BOOL=TRUE
 
 cmake --build llvm-build/ --config Release
 
